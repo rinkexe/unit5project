18992b17ebe8bad4de25a78b0ac73180bd5747ba
rinkexe <rinkexe@icloud.com>
2026-03-13 10:10:12 -0400

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,4 @@
 scene.setBackgroundImage(assets.image`Background`)
 let playerSprite = sprites.create(assets.image`Planeimg`, SpriteKind.Player)
+controller.moveSprite(playerSprite, 0, 100)
+playerSprite.setPosition(25, 60)
