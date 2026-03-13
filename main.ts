70be3bf12aa199ba33bd28f78e7aefa55c75cb9e
rinkexe <rinkexe@icloud.com>
2026-03-13 11:08:57 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,30 @@
+function initAlien (num: number) {
+    for (let index = 0; index < num; index++) {
+        sprites.createProjectileFromSide(img`
+            . . . . . c c c c c c c . . . . 
+            . . . . c 6 7 7 7 7 7 6 c . . . 
+            . . . c 7 c 6 6 6 6 c 7 6 c . . 
+            . . c 6 7 6 f 6 6 f 6 7 7 c . . 
+            . . c 7 7 7 7 7 7 7 7 7 7 c . . 
+            . . f 7 8 1 f f 1 6 7 7 7 f . . 
+            . . f 6 f 1 f f 1 f 7 7 7 f . . 
+            . . . f f 2 2 2 2 f 7 7 6 f . . 
+            . . c c f 2 2 2 2 7 7 6 f c . . 
+            . c 7 7 7 7 7 7 7 7 c c 7 7 c . 
+            c 7 1 1 1 7 7 7 7 f c 6 7 7 7 c 
+            f 1 1 1 1 1 7 6 f c c 6 6 6 c c 
+            f 1 1 1 1 1 1 6 6 c 6 6 6 c . . 
+            f 6 1 1 1 1 1 6 6 6 6 6 6 c . . 
+            . f 6 1 1 1 1 1 6 6 6 6 c . . . 
+            . . f f c c c c c c c c . . . . 
+            `, randint(-10, -20), 0).setPosition(160, randint(10, 110))
+    }
+}
 scene.setBackgroundImage(assets.image`Background`)
+info.setScore(0)
 let playerSprite = sprites.create(assets.image`Planeimg`, SpriteKind.Player)
 controller.moveSprite(playerSprite, 0, 100)
 playerSprite.setPosition(25, 60)
+game.onUpdateInterval(2000, function () {
+    initAlien(randint(1, 3))
+})
